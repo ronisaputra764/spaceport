a689b010e8382d74ae76675929d4141c3be4e8e7
ronisaputra764 <ronisaputra764@gmail.com>
2025-04-13 02:27:12 +0700

    first commit

--- src/components/DesktopHeader.jsx
+++ src/components/DesktopHeader.jsx
@@ -10,7 +10,7 @@ export default function DesktopHeader() {
             id="home"
             className="flex flex-row px-20 mt-40 z-[20] w-full 2xl:w-[80%] mx-auto"
         >
-            <div className="h-full text-[#c9c1e3] flex-col gap-5 justify-center grid grid-cols-2 mt-44 text-start">
+            <div className="h-full px-5 text-[#c9c1e3] flex-col gap-5 justify-center grid grid-cols-2 mt-44 text-start">
 
                 <div className="mt-32">
 
@@ -24,7 +24,7 @@ export default function DesktopHeader() {
                         scale={1}
                         threshold={0}
                     >
-                        <div className="mb-4 border rounded-full py-[8px] px-[12px] opacity-[0.9] inline-block border-[#7042F88B] shadow-[inset_-5px_-5px_10px_0_rgba(59,130,246,0.3)]">
+                        <div className="mb-4 ms-4 border rounded-full py-[8px] px-[12px] opacity-[0.9] inline-block border-[#7042F88B] shadow-[inset_-5px_-5px_10px_0_rgba(59,130,246,0.3)]">
                             <div className="flex fony-semibold items-center bg-clip-text bg-gradient-to-r text-transparent from-purple-500 to-cyan-200 ">
                                 <img className="mr-[10px] h-5 w-5" src="/assets/sparkles.svg" alt="" />
                                 <h1 className="text-[13px] text-nowrap ">Fullstack Web Developer</h1>
@@ -43,7 +43,7 @@ export default function DesktopHeader() {
                         scale={1}
                         threshold={0.2}
                     >
-                        <h1 className="bg-clip-text inline w-full text-7xl poppins-semibold bg-gradient-to-r text-transparent from-purple-500 to-cyan-500 [text-shadow:0_0_500px_#7042F88B,0_0_100px_#a855f7]">Roni Saputra</h1>
+                        <h1 className="bg-clip-text inline w-full ms-4 text-7xl poppins-semibold bg-gradient-to-r text-transparent from-purple-500 to-cyan-500 [text-shadow:0_0_500px_#7042F88B,0_0_100px_#a855f7]">Roni Saputra</h1>
                     </AnimatedContent>
 
                     <AnimatedContent
@@ -57,7 +57,7 @@ export default function DesktopHeader() {
                         scale={1}
                         threshold={0.2}
                     >
-                        <p className=" text-[#c3c3c3] text-xs sm:text-base mt-2 transition-all duration-700">I'm a web developer specializing in creating dynamic and user-friendly websites.
+                        <p className="ms-4 text-[#c3c3c3] text-xs sm:text-base mt-2 transition-all duration-700">I'm a web developer specializing in creating dynamic and user-friendly websites.
                             I have experience in front-end and back-end development, along with a passion
                             for crafting intuitive user interfaces and efficient code
                         </p>
@@ -74,17 +74,17 @@ export default function DesktopHeader() {
                         scale={1}
                         threshold={0.2}
                     >
-                        <div className="flex gap-3 mt-4">
-                            <a className="flex items-center bg-white/5 border border-gray-700 hover:cursor-pointer text-sm py-2 px-4 rounded-full">
+                        <div className="flex gap-3 ms-4 mt-4">
+                            <a className="flex items-center bg-white/5 border border-gray-700 text-sm py-2 px-4 rounded-full">
                                 <span>React</span>
                             </a>
-                            <a className="flex items-center bg-white/5 border border-gray-700 hover:cursor-pointer text-sm py-2 px-4 rounded-full">
+                            <a className="flex items-center bg-white/5 border border-gray-700 text-sm py-2 px-4 rounded-full">
                                 <span>Javascript</span>
                             </a>
-                            <a className="flex items-center bg-white/5 border border-gray-700 hover:cursor-pointer text-sm py-2 px-4 rounded-full">
+                            <a className="flex items-center bg-white/5 border border-gray-700 text-sm py-2 px-4 rounded-full">
                                 <span>Node.js</span>
                             </a>
-                            <a className="flex items-center bg-white/5 border border-gray-700 hover:cursor-pointer text-sm py-2 px-4 rounded-full">
+                            <a className="flex items-center bg-white/5 border border-gray-700 text-sm py-2 px-4 rounded-full">
                                 <span>Tailwind</span>
                             </a>
                         </div>
@@ -101,14 +101,14 @@ export default function DesktopHeader() {
                         scale={1}
                         threshold={0.2}
                     >
-                        <div className="flex gap-12 mt-6">
+                        <div className="flex gap-12  px-5 mt-6 mb-6">
                             <div className="bg-black inline">
                                 <div className="relative inline-flex group">
                                     <div
-                                        className="absolute transitiona-all duration-200 opacity-70 -inset-px bg-gradient-to-r from-cyan-500 via-purple-500 to-[#FF44EC]  rounded-xl blur-md group-hover:opacity-100 group-hover:-inset-1 ">
+                                        className="absolute transitiona-all duration-100 opacity-70 -inset-px bg-gradient-to-r from-cyan-500 via-purple-500 to-[#FF44EC]  rounded-xl blur-md group-hover:opacity-100 group-hover:-inset-1 ">
                                     </div>
-                                    <a href="CV_Roni_Saputra.pdf" title="Get quote now" target="_blank"
-                                        className="relative text-nowrap inline-flex items-center justify-center px-8 py-4 text-md font-semibold text-white transition-all duration-200 bg-[#030014] font-pj rounded-xl focus:outline-none "
+                                    <a href="CV_Roni_Saputra.pdf" download="CV_Roni_Saputra.pdf" title="Get quote now" target="_blank"
+                                        className="relative text-nowrap inline-flex items-center justify-center px-8 py-4 text-md font-semibold text-white transition-all duration-100 bg-[#030014] font-pj rounded-xl focus:outline-none "
                                         role="button">Download CV
                                     </a>
                                 </div>
@@ -120,7 +120,7 @@ export default function DesktopHeader() {
                                         className="absolute transitiona-all duration-200 opacity-70 -inset-px bg-gradient-to-r from-cyan-500 via-purple-500 to-[#FF44EC]  rounded-xl blur-md group-hover:opacity-100 group-hover:-inset-1 animate-tilt">
                                     </div>
                                     <a href="mailto:ronisaputra764@gmail.com?" title="Get quote now"
-                                        className="relative text-nowrap  inline-flex items-center justify-center px-8 py-4 text-md font-semibold text-white transition-all duration-200 bg-[#030014] font-pj rounded-xl focus:outline-none "
+                                        className="relative text-nowrap  inline-flex items-center justify-center px-8 py-4 text-md font-semibold text-white transition-all duration-100 bg-[#030014] font-pj rounded-xl focus:outline-none "
                                         role="button">Send Me Email
                                     </a>
                                 </div>
